97e78c800d6cabc7d791cb5c8b2802d1193a630a
michaelrhodes <spam@michaelrhod.es>
2016-05-13 22:35:32 -0700

    Remove legacy regexp stuff

--- index.js
+++ index.js
@@ -5,14 +5,9 @@ module.exports = function (args) {
   var xargs = slice.call(args)
 
   return function (type) {
-    // Including word boundaries will prevent
-    // Array from matching ArrayBuffer, etc.
-    var typex = RegExp('\\b' + type + '\\b', 'i')
-
     var i = xargs.length
     while (i--) {
-      v = xargs[i]
-      if (typex.test(t(v))) {
+      if (t(v = xargs[i]) === type) {
         // Remove the found item from the args
         // to speed up subsequent `arg-find` calls.
         return xargs.splice(i, 1)[0]
